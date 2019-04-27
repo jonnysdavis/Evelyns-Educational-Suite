1435f0012d1ed0df86bd0a172935626acb293f40
Jonathan <jonnysdavis@gmail.com>
2019-04-27 18:36:17 -0400

    expaned file menu

--- Student.py
+++ Student.py
@@ -1,31 +1,27 @@
 #This class is an object to contain Student info when using them during the program
 
 
-lastName = ""
-firstName = ""
-class_period = 0
-grade = 0
-other = [[""][""]]
+
 banList = []  # A List of all the students the student in question should not work with
 
 class Student(object):
+    lastName = ""
+    firstName = ""
+    class_period = 0
+    grade = 0
 
-    def __init__(last,first,class_period_arg,grade_arg):
+    def __init__(last,first,class_period_arg):
         lastName = last
         firstName = first
         class_period = class_period_arg
-        grade = grade_arg
 
     def addAttrbuteAll(attribute,roster): #Add an attribute to each student
         for Student in roster:
             tempStudent = Student
-            tempStudent.
 
     def addBan(Student):
         banList.append(Student)
 
-    def addAttribute(attribute):
-        other.append([attribute]["?"])
 
 
 
--- evelyn.py
+++ evelyn.py
@@ -10,6 +10,13 @@ window.title("Evelyn's Educational Suite")
 menu = Menu(window)
 new_item = Menu(menu)
 new_item.add_command(label='About')
+new_item.add_command(label='Save')
+new_item.add_command(label='Exit')
 menu.add_cascade(label='File', menu=new_item)
 window.config(menu=menu)
 window.mainloop()
+
+def addStudent(last, first):
+    temp = Student("doe","john",1)
+    roster.append(temp)
+
